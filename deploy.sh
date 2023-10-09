71742b8c208617f51b89d56fb1bb07f3ff718f1d
yongj.zhuang <zhuangyongj@gmail.com>
2023-10-09 23:30:27 +0800

    Modified:  deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ build="fbackuprun"
 CGO_ENABLED=0 GOOS="$os" GOARCH="$arch" go build -o "$build"
 
 [[ $? -eq 0 ]] && scp fbackuprun alphaboi@curtisnewbie.com:/home/alphaboi/services/fstore_backup
-
+[[ -f "$build" ]] && rm "$build"
